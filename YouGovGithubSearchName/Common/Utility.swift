25fc4ced970dfa93b41d761309e2ff49672d6157
Dileep Kumar Jaiswal <dileep.ducs@gmail.com>
2021-11-23 12:38:54 +0530

    Added common file

---
+++ YouGovGithubSearchName/Common/Utility.swift
@@ -0,0 +1,21 @@
+//
+//  Utility.swift
+//  YouGovGithubSearchName
+//
+//  Created by Dileep Jaiswal on 21/11/21.
+//
+
+import UIKit
+
+class Utility {
+    
+    // Screen width.
+    public static var screenWidth: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+
+    // Screen height.
+    public static var screenHeight: CGFloat {
+        return UIScreen.main.bounds.height
+    }
+}
